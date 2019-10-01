e57dc4919db1b2837a96f5d2ecddb27907bcc7a3
helenaKertesz <helena.kertesz1@gmail.com>
2019-10-01 11:19:45 +1000

    wk03

---
+++ wk03/addition.c
@@ -0,0 +1,19 @@
+// COMP1511 Lab 02 Exercise - Addition
+//
+// Add
+//
+// Authors:
+// Helena Kertesz (z5059868@unsw.edu.au)
+//
+// Written: 29/09/2019
+
+int main(void){
+
+    printf("Please enter two integers: ");
+    scanf("%d", &a );
+    scanf("%d", &b );
+
+    printf("%d + %d = %d\n",a,b,a+b);
+
+    return 0;
+}
---
+++ wk03/cm2feet.c
@@ -0,0 +1,17 @@
+// COMP1511 Tut 03  Exercise - cm to feet
+// Give a height in cm convert it to a height in feet
+// there are 2.54 cm in and inch and 12 inches in a foot
+//
+// Use only INT variables
+//
+// Authors: tue11-brass
+// Written: 01/10/2019
+//
+
+#include <stdio.h>
+
+int main(void){
+
+
+    return 0;
+}
